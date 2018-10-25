0f5332ed7b481544689cef99ddb814dc31d14e14
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-25 09:59:01 +0200

    Initial commit

---
+++ src/ApproximatePI.java
@@ -0,0 +1,10 @@
+public class ApproximatePI {
+    public static void main(String[] args)   {
+        // we need to built two print commands which display each their version calculation of PI
+        // First one
+        System.out.println(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11));
+
+        // Second one
+        System.out.println(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13));
+    }
+}
